22ff8671857ac4b61089c068643bbb4912871f6e
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-09-27 19:34:24 +0300

    add video to show detail item

--- src/app/pages/show-detail/show-detail.component.ts
+++ src/app/pages/show-detail/show-detail.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { Actor } from 'src/app/modules/credits';
 import { Movie } from 'src/app/modules/movie';
+import { Video } from 'src/app/modules/video';
+import { Image } from 'src/app/modules/image';
 import { MoviesService } from 'src/app/services/movies.service';
 import { TvshowsService } from 'src/app/services/tvshows.service';
+import { mapToMovie, mapToMovies } from 'src/app/modules/tvshows';
+import { IMAGES_SIZES } from 'src/app/constants/images-sizes';
 
 @Component({
   selector: 'app-show-detail',
@@ -15,6 +20,12 @@ export class ShowDetailComponent implements OnInit {
   showType: 'tv' | 'movie' = 'movie';
 
   show$: Observable<Movie> | null = null;
+  showVideos$: Observable<Video[]> | null = null;
+  showImages$: Observable<Image[]> | null = null;
+  showCast$: Observable<Actor[]> | null = null;
+  similarShows$: Observable<Movie[]> | null = null;
+
+  imagesSizes = IMAGES_SIZES;
 
   constructor(
     private router: ActivatedRoute,
@@ -24,7 +35,25 @@ export class ShowDetailComponent implements OnInit {
 
   ngOnInit() {
     this.showId = this.router.snapshot.params['id'];
-    this.show$ = this.moviesService.getMovieById(this.showId)
-   
+    this.showType = this.router.snapshot.params['type'];
+
+    if (this.showType === 'movie') {
+      this.show$ = this.moviesService.getMovieById(this.showId);
+      this.showVideos$ = this.moviesService.getMovieVideos(this.showId);
+      this.showImages$ = this.moviesService.getMovieImages(this.showId);
+      this.showCast$ = this.moviesService.getMovieCast(this.showId);
+      this.similarShows$ = this.moviesService.getMovieSimilar(this.showId);
+    }
+    if (this.showType === 'tv') {
+      this.show$ = this.tvService
+        .getTvShowById(this.showId)
+        .pipe(map(mapToMovie));
+      this.showVideos$ = this.tvService.getTvShowVideos(this.showId);
+      this.showImages$ = this.tvService.getTvShowImages(this.showId);
+      this.showCast$ = this.tvService.getTvShowCast(this.showId);
+      this.similarShows$ = this.tvService
+        .getTvShowSimilar(this.showId)
+        .pipe(map(mapToMovies));
+    }
   }
 }
